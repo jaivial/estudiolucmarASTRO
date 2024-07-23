8975d8d771b9f867a9df802ce3181e60ac79aa0a
jaivial <114494844+jaivial@users.noreply.github.com>
2024-07-23 14:14:44 +0200

    yes

--- deploy.sh
+++ deploy.sh
@@ -3,20 +3,3 @@
 # Change directory to 'dist'
 cd dist
 
-git init
-
-git remote add origin https://github.com/jaivial/dist.git
-
-# Add all changes to the staging area
-git add . -f
-
-# Commit changes with a message
-git commit -m "deployment build"
-
-# Push changes to the remote 'dist-branch'
-git push origin main
-
-# Return to the previous directory
-cd ..
-
-git init
