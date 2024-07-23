4ba78196f6798a53cc70b1279d59c6eac8f2ef0a
jaivial <114494844+jaivial@users.noreply.github.com>
2024-07-23 14:30:39 +0200

    ss

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+set -e
 
-# Change directory to 'dist'
+# Work in the root repository
+echo "Working in the root repository"
+cd .
+
+# Change to the 'dist' directory
+echo "Changing to 'dist' directory"
 cd dist
 
+# Work in the nested 'dist' repository
+echo "Working in the 'dist' repository"
