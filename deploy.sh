a3b636140c332d3b78ba8c767dfc9f7201314ca3
jaivial <114494844+jaivial@users.noreply.github.com>
2024-07-23 15:29:02 +0200

    yes

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-# Add all changes to the staging area
-git add .
+# # Add all changes to the staging area
+# git add .
 
-# Commit the changes with a message
-git commit -m "deployment"
+# # Commit the changes with a message
+# git commit -m "deployment"
 
-# Push the changes to the main branch on the origin remote
-git push origin main
+# # Push the changes to the main branch on the origin remote
+# git push origin main
 
-# Switch to the dist branch
-git checkout dist
+# # Switch to the dist branch
+# git checkout dist
 
-# Merge changes from the main branch into the dist branch
-git merge main
+# # Merge changes from the main branch into the dist branch
+# git merge main
 
-# Stage only changes from the dist directory
-git add dist
+# # Stage only changes from the dist directory
+# git add dist
 
-# Commit the changes with a message
-git commit -m "Update dist directory after merging main"
+# # Commit the changes with a message
+# git commit -m "Update dist directory after merging main"
 
-# Push the changes to the dist branch on the origin remote
-git push dist-branch dist
+# # Push the changes to the dist branch on the origin remote
+# git push dist-branch dist
 
-# Switch back to the main branch
-git checkout main
+# # Switch back to the main branch
+# git checkout main
